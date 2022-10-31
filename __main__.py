d0334a21e08f4470f8d4d7705bd60ba9f81fd2cc
shunichi <shunichi1100921@icloud.com>
2022-10-31 20:17:50 +0900

    Touch __init__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-import molecule_synthesizer.controller.synthesis_multi_fragment
+import molecule_synthesizer.controller.synthesis
 
 
 if __name__ == '__main__':
-    molecule_synthesizer.controller.synthesis_multi_fragment.synthesize_multiple_molecules()
+    molecule_synthesizer.controller.synthesis.synthesize_multiple_molecules()
