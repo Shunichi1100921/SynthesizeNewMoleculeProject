b93ce6434dcc920c8816459f3ffbd0e0d86a334a
shunichi <shunichi1100921@icloud.com>
2022-11-07 20:06:16 +0900

    Create settings.py which describe fragments.

---
+++ settings.py
@@ -0,0 +1,6 @@
+fragments = {'benzothiazole': 'F4',
+             'amide': 'F1',
+             'aryl': 'F5',
+             'alcohol1': 'F2',
+             'alcohol2': 'F25',
+             'modifier': 'F13'}
